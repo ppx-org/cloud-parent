c3b63bd2fa79c49c4aae92d6d03e9466597eb135
dengxz <13602493280@qq.com>
2018-02-01 12:58:11 +0800

    t

--- docs/base.sql
+++ docs/base.sql
@@ -1,24 +1,42 @@
 
 
+/* -------------------------content----------home---------------- */
+create table home_swiper
+(
+	SWIPER_ID	int(11) NOT NULL auto_increment,
+	STORE_ID 	int(11) NOT NULL,
+	SWIPER_IMG 	varchar(128) NOT NULL,
+	SWIPER_URL 	varchar(128) NOT NULL,
+	SWIPER_PRIO	int not null,
+	CREATED     timestamp not null default CURRENT_TIMESTAMP,
+	primary key (SWIPER_ID)
+);
 
+create table home_level
+(
+	LEVEL_ID 	int(11) NOT NULL auto_increment,
+	STORE_ID 	int(11) NOT NULL,
+	LEVEL_NAME	varchar(32) NOT NULL,
+	LEVEL_PRIO 	int not null,
+	CREATED     timestamp not null default CURRENT_TIMESTAMP,
+	primary key (LEVEL_ID)
+);
 
+create table home_level_product
+(
+	LEVEL_ID 	int(11) NOT NULL,
+	PROD_ID 	int(11) NOT NULL,
+	PROD_PRIO 	int not null,
+	CREATED     timestamp not null default CURRENT_TIMESTAMP,
+	primary key (LEVEL_ID, PROD_ID)
+);
 
-
-
-
-
-
-
-
-
-/******************* img *******************/
-
-create table img
+create table home_img
 (
 	MERCHANT_ID int(11) NOT NULL,
 	IMG_TYPE 	varchar(32) NOT NULL,
 	IMG_PRIO	int(11) NOT NULL,
-	IMG_URL		varchar(256),
+	IMG_SRC		varchar(256),
 	primary key (MERCHANT_ID, IMG_TYPE)
 );
 
@@ -31,24 +49,6 @@ create table img
 
 
 /******************* search ****************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 create table search_normal_v1
 (
@@ -169,38 +169,7 @@ create table search_last_word
 
 
 
-/* -------------------------content----------home---------------- */
 
-
-create table home_swiper
-(
-	SWIPER_ID	int(11) NOT NULL auto_increment,
-	STORE_ID 	int(11) NOT NULL,
-	SWIPER_IMG 	varchar(128) NOT NULL,
-	SWIPER_URL 	varchar(128) NOT NULL,
-	SWIPER_PRIO	int not null,
-	CREATED     timestamp not null default CURRENT_TIMESTAMP,
-	primary key (SWIPER_ID)
-);
-
-create table home_level
-(
-	LEVEL_ID 	int(11) NOT NULL auto_increment,
-	STORE_ID 	int(11) NOT NULL,
-	LEVEL_NAME	varchar(32) NOT NULL,
-	LEVEL_PRIO 	int not null,
-	CREATED     timestamp not null default CURRENT_TIMESTAMP,
-	primary key (LEVEL_ID)
-);
-
-create table home_level_product
-(
-	LEVEL_ID 	int(11) NOT NULL,
-	PROD_ID 	int(11) NOT NULL,
-	PROD_PRIO 	int not null,
-	CREATED     timestamp not null default CURRENT_TIMESTAMP,
-	primary key (LEVEL_ID, PROD_ID)
-);
 
 
 
